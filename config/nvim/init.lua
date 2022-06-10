a66929ce68c7a135afd638692d1375382c9cd2ff
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-06-10 08:32:19 +0200

    if nvim_tree is the last open buffer, close entirely

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -57,3 +57,12 @@ vim.cmd[[
   endfunction
 ]]
 
+-- Autoclose: https://github.com/kyazdani42/nvim-tree.lua/discussions/1115
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function ()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
